bdb647c921968cd65b485dbca76f70d903314e7e
Roger Zurawicki <rzurawicki@gmail.com>
2013-09-12 19:04:11 -0400

    Made the thing compile, memory stuff is unit tested

--- mips_sim.ml
+++ mips_sim.ml
@@ -34,30 +34,30 @@ type state = { r : regfile; pc : int32; m : memory }
 
 (* This shifts the current bits to the left by length and adds value to it *)
 let push (onto : int32) (length : int) (value : int32) : int32 = 
-  Int32.logor (Int32.shift_left onto length) value
+  Int32.logor (Int32.shift_left onto length) value 
 
-assert ( push 1l 5 1l = 33l ) ;
+let _ = assert ( push 1l 5 1l = 33l ) 
 
 (* Helper function for registers specifically *)
 let push_reg (onto : int32) (r : reg) = 
-  push onto 5 Int32.of_int (reg2ind r)
+  push onto 5 (Int32.of_int (reg2ind r))
 
 (* Converts an instruction into the corresponding encoding *)
 let rec inst2word (instr : inst) : int32 = 
   match instr with
   | Add (rd, rs, rt) -> 
-    let val1 = push_reg 0x0l rs;
-    let val2 = push_reg val1 rt;
-    let val3 = push_reg val2 rd;
-    let val4 = push val3 5 0x0l;
+    let val1 = push_reg 0x0l rs in
+    let val2 = push_reg val1 rt in
+    let val3 = push_reg val2 rd in
+    let val4 = push val3 5 0x0l in
     push val4 6 0x20l
   | Beq (rs, rt, offset) ->
-    let val1 = push_reg 0x4l rs;
-    let val2 = push_reg val1 rt;
+    let val1 = push_reg 0x4l rs in
+    let val2 = push_reg val1 rt in
     push val2 16 offset
   | Jr (rs) ->
-    let val1 = push_reg 0x0l rs;
-    let val2 = push val1 15 0x0l;
+    let val1 = push_reg 0x0l rs in
+    let val2 = push val1 15 0x0l in
     push val2 6 0x8l
   | Jal (target) ->
     push 0x3l 26 target
@@ -66,41 +66,44 @@ let rec inst2word (instr : inst) : int32 =
   | Li (rdest, imm) ->
     inst2word (Lui (rdest, Int32.shift_right imm 16)) (* you need to do an ori after this! *)
   | Lui (rt, imm) -> 
-    let val1 = push 0xf 5 0x0l;
-    let val2 = push_reg val1 rt;
-    push val2 16 (Int32.shift_right imm)
+    let val1 = push 0xfl 5 0x0l in
+    let val2 = push_reg val1 rt in
+    push val2 16 (Int32.logand 0xFFFFl imm)
   | Ori (rt, rs, imm) ->
-    let val1 = push_reg 0xd rs;
-    let val2 = push_reg val1 rt;
-    push val2 16 (Int32.logand 0xFFFF imm)
+    let val1 = push_reg 0xdl rs in
+    let val2 = push_reg val1 rt in
+    push val2 16 (Int32.logand 0xFFFFl imm)
   | Lw (rt, rs, offset) ->
-    let val1 = push_reg 0x23 rs;
-    let val2 = push_reg val1 rt;
+    let val1 = push_reg 0x23l rs in
+    let val2 = push_reg val1 rt in
     push val2 16 offset
   | Sw (rt, rs, offset) -> 
-    let val1 = push_reg 0x2b rs;
-    let val2 = push_reg val1 rt;
-    push val2 = 16 offset
+    let val1 = push_reg 0x2bl rs in
+    let val2 = push_reg val1 rt in
+    push val2 16 offset
 
 (* TODO test *) (* Adds an encoding to memory *)
 let mem_update_word (a : int32) (word : int32) (m : memory) : memory =
-  let byte0 = mk_byte word;
-  let byte1 = mk_byte (Int32.shift_right_logical word 8);
-  let byte2 = mk_byte (Int32.shift_right_logical word 16);
-  let byte3 = mk_byte (Int32.shift_right_logical word 24);
-  mem_update (Int32.add a 0l) byte0 m;
-  mem_update (Int32.add a 1l) byte1 m;
-  mem_update (Int32.add a 2l) byte2 m;
-  mem_update (Int32.add a 3l) byte3 m
+  let byte0 = mk_byte word in
+  let byte1 = mk_byte (Int32.shift_right_logical word 8) in
+  let byte2 = mk_byte (Int32.shift_right_logical word 16) in
+  let byte3 = mk_byte (Int32.shift_right_logical word 24) in
+  let m0 = mem_update (Int32.add a 0l) byte0 m in
+  let m1 = mem_update (Int32.add a 1l) byte1 m0 in
+  let m2 = mem_update (Int32.add a 2l) byte2 m1 in
+  mem_update (Int32.add a 3l) byte3 m2
 
 (* Map a program, a list of Mips assembly instructions, down to a starting 
    state. You can start the PC at any address you wish. Just make sure that 
    you put the generated machine code where you started the PC in memory! *)
 let rec assem (prog : program) : state = 
-  let rec load (prog : program) (mem : memory) (pos : int32) : memory= 
+  let rec load (prog : program) (mem : memory) (pos : int32) : memory = 
     match prog with
     | instr::prog ->
-      load prog (mem_update_word pos inst2word(instr) mem) (Int32.add pos 4l)
+      load 
+      prog 
+      (mem_update_word pos (inst2word instr) mem) 
+      (Int32.add pos 4l)
     | [] -> mem
   in
   let pos : int32 = 0x100l in 
@@ -108,18 +111,17 @@ let rec assem (prog : program) : state =
 
 (* TODO test *)
 let mem_lookup_word (a : int32) (m : memory) : int32 =
-  let byte0 = mem_lookup (Int32.add a 0l) m;
-  let byte1 = mem_lookup (Int32.add a 1l) m;
-  let byte2 = mem_lookup (Int32.add a 2l) m;
-  let byte3 = mem_lookup (Int32.add a 3l) m;
-
-  let val = push (b2i32 (byte3)) 8 (b2i32 (byte2)) in
-  let val = push val 8 (b2i32 (byte1)) in
-  push val 8 (b2i32 (byte0))
+  let byte0 = mem_lookup (Int32.add a 0l) m in
+  let byte1 = mem_lookup (Int32.add a 1l) m in
+  let byte2 = mem_lookup (Int32.add a 2l) m in
+  let byte3 = mem_lookup (Int32.add a 3l) m in
+  let word = push (b2i32 (byte3)) 8 (b2i32 (byte2)) in
+  let word = push word 8 (b2i32 (byte1)) in
+  push word 8 (b2i32 (byte0))
 
 let word2inst (word: int32) : inst = raise TODO
 
-let run_inst (i : inst) (s: state) : state = (* TODO *)
+let run_inst (i : inst) (s: state) : state = s(* TODO 
   match i with
     | Add (rd, rs, rt) -> 
       let sum = Int32.add (rf_lookup rs s.r) (rf_lookup rt s.r) in
@@ -131,15 +133,22 @@ let run_inst (i : inst) (s: state) : state = (* TODO *)
     | Lui (rt, imm) -> 
     | Ori (rt, rs, imm) ->
     | Lw (rt, address, offset) -> 
-    | Sw (rt, address, offset) ->;
+    | Sw (rt, address, offset) ->; *)
+
+(* Testing memory word functions *)
+let _ =
+  let addr = 0x0f0f0f0fl in
+  let word = 0xf0f0f0f0l in
+  let m = mem_update_word addr word empty_mem in
+    assert ( mem_lookup_word addr m = word)
 
 (* Given a starting state, simulate the Mips machine code to get a final state *)
 let rec interp (init_state : state) : state = 
   if init_state.pc == 0x0l then
     init_state
   else
-    let word : int32 = mem_lookup_word pc in
+    let word : int32 = mem_lookup_word init_state.pc init_state.m in
     let instr : inst = word2inst word in
-    let new_state = run_inst instr init_state;
+    let new_state = run_inst instr init_state in
     interp new_state
 
