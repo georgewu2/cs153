7cb7e30d9f3ae5d0df84117388c6b61ba8520808
Roger Zurawicki <rzurawicki@gmail.com>
2013-09-26 14:49:34 -0400

    Implemented comblexer and all tokens
     - Also tidy up'd parse.mly and lex.mll

--- ps1/comblexer.ml
+++ ps1/comblexer.ml
@@ -4,13 +4,16 @@ open Lcombinators.CharParsing
 (* the datatype for tokens -- you will need to augment these *)
 type token = 
     INT of int 
-  | PLUS | MINUS | STAR | SLASH
-  | LPAREN | RPAREN
-  | EQUAL | NEQ | GTE | LTE | GT | LT
-  | WHITESPACE | COMMENT 
-  | SEMI
-  | RETURN
+  | VAR of string
   | EOF
+  | RETURN | IF | ELSE | WHILE | FOR
+  | PLUS | MINUS | STAR | SLASH
+  | LPAREN | RPAREN | LBRACE | RBRACE
+  | EQUAL | NEQ | LT | LTE | GT | GTE
+  | NOT | AND | OR
+  | ASSIGN | SEMI
+  | WHITESPACE | COMMENT 
+
 
 (* removes WHITESPACE and COMMENT tokens from a token list *)
 let remove_whitespace (ts: token list) : token list =
@@ -23,25 +26,24 @@ let rec tokenize(cs:char list) : token list =
   let ws_parser = const_map WHITESPACE white in
   let comment_parser = const_map COMMENT comment in
   let int_parser = map (fun i -> INT i) integer in
-  let plus_parser = const_map PLUS (c '+') in
-  let minus_parser = const_map MINUS (c '-')  in
-  let star_parser = const_map STAR (c '*')  in
-  let slash_parser = const_map SLASH (c '/')  in
-  let lparen_parser = const_map LPAREN (c '(') in
-  let rparen_parser = const_map RPAREN (c ')') in
-  let equal_parser = const_map EQUAL (str "==") in 
-  let neq_parser = const_map NEQ (str "!=") in
-  let gte_parser =  const_map GTE (str ">=") in
-  let lte_parser = const_map LTE (str "<=") in
-  let gt_parser = const_map GT (c '>') in
-  let lt_parser = const_map LT (c '<') in
-  let return_parser = const_map RETURN (str "return") in
-  let semi_parser = const_map SEMI (c ';') in
-  let all_tokens = [int_parser; ws_parser; comment_parser; 
-    plus_parser; minus_parser; star_parser; slash_parser;
-    lparen_parser; rparen_parser; equal_parser; neq_parser;
-    gte_parser; lte_parser; gt_parser; lt_parser; 
-    return_parser; semi_parser] in
+  let id_parser = map (fun i -> VAR i) identifier in
+
+  let str_pairs = 
+  [  (str "return", RETURN); (str "if", IF); (str "else", ELSE);
+  (str "while", WHILE); (str "for", FOR); (str "==", EQUAL); (str "!=", NEQ);
+  (str ">=", GTE); (str "<=", LTE); (str "&&", AND); (str "||", OR); ] in
+  let c_pairs = 
+  [ (c '+', PLUS); (c '-', MINUS); (c '*', STAR); (c '/', SLASH);
+  (c '(', LPAREN); (c ')', RPAREN); (c '{', LBRACE); (c '}', RBRACE);
+  (c '>', GT); (c '<', LT); (c '!', NOT); (c '=', ASSIGN); (c ';', SEMI); ] in
+
+  let str_parser = 
+    List.map (function (code, t) -> const_map t code) str_pairs in
+  let c_parser = 
+    List.map (function (code, t) -> const_map t code) c_pairs in
+
+  let all_tokens = [ws_parser; comment_parser; 
+  int_parser; (alts c_parser); (alts str_parser); id_parser] in
   let eof_parser = map (fun _ -> EOF) eof in
   let p = seq (star (alts all_tokens), eof_parser) in
   match run (p cs) with
