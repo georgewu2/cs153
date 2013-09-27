8f09ece82aa3c6cc03b699fd88848dcecc67ab64
George Wu <georgewu@college.harvard.edu>
2013-09-27 13:54:10 -0400

    made code run 5x faster

--- ps1/combparser.ml
+++ ps1/combparser.ml
@@ -13,9 +13,7 @@ let rec make_exp_parser (():unit) : (token, exp) parser =
     lazy_seq (lazy (make_gexp_parser ()), lazy (satisfy (fun t -> t == RPAREN)))) in 
   let sub_exp_parser = map (fun (_, (e, _)) -> e) sub_parser in
   let first_parser = alts [int_parser; var_parser; sub_exp_parser] in
-  let rest_parser = seq (first_parser, make_dexp_rest ()) in
-  let binop_parser = map (fun (e1, (op, e2)) -> (Binop (e1, op, e2), dummy_pos)) rest_parser in
-  alts [binop_parser; first_parser]
+  first_parser
 
 and make_aexp_parser (():unit) : (token, exp) parser = 
   let rest_parser = make_aexp_rest () in
