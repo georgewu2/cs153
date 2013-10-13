408b57b3e9d13191af04bfa6d987b8e764f3e953
George Wu <georgewu@college.harvard.edu>
2013-10-13 13:50:19 -0400

    some compiling stuff

--- ps3/compile.ml
+++ ps3/compile.ml
@@ -11,19 +11,25 @@ let label_counter = ref 0
 let new_int() = (label_counter := (!label_counter) + 1; !label_counter)
 let new_label() = "L" ^ (string_of_int (new_int()))
 
+let sp = R29
+let fp = R30
+let ra = R31
+
 let rec compile (p:Ast.program) : result =
 	match p with
 	| hd::tl -> 
 	| [] -> 
-let allocate_word : inst = Add (R29, R29, Immed -4l) in
+let allocate_word : inst = Add (sp, sp, Immed -4l) in
 
+(* we need to make an environment *)
 let compile_function (f:funcsig) : inst list = 
 	let make_prologue (():unit) : inst list = 
-		allocate_word::Sw(R31, R29, 0l)::allocate_word::Sw(R30, R29, 0l)::
-		Add(R30, R29, Immed 4l)::[] in
+		allocate_word::Sw(ra, sp, 0l)::allocate_word::Sw(fp, sp, 0l)::
+		Add(fp, sp, Immed 4l)::Sw(R4, fp, Immed 4l)::Sw(R5, fp, Immed 8l)::
+		Sw(R6, fp, Immed 12l)::(R7, fp, Immed 16l)::[] in
 	let make_epilogue (():unit) : inst list = 
-		Lw(R31, R30, Immed 0l)::Lw(R30, R30, Immed 4l)::
-		Add(R29, R29, Immed value)::[] in
+		Lw(ra, fp, Immed 0l)::Lw(fp, fp, Immed 4l)::
+		Add(sp, sp, Immed value)::Jr(ra)[] in
 	let compile_stmt ((s, _):Ast.stmt) : inst list = 
 		
 	let make_body (f:funsig) : inst list = 
